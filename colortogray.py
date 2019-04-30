1f6cd5f3b0c202698c17b9822b5738561953e16c
valleygtc <valleygtc@valleygtcdeMacBook-Air.local>
2019-04-30 14:38:35 +0800

    clean files

--- colortogray.py
+++
@@ -1,5 +0,0 @@
-import cv2 as cv
-img = cv.imread('img/lena_std.tif', cv.IMREAD_GRAYSCALE)
-cv.imshow('lena', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
--- try_matplot.py
+++
@@ -1,6 +0,0 @@
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib import pyplot as plt
-
-fig, ax_lst = plt.subplots(2, 2)
-plt.show()
